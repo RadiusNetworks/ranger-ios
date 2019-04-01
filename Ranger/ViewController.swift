03e74b2098c4f7f2f0e7b9bd4f6d216ee53fe3f3
Michael Harper <mharper@standalonecode.com>
2019-04-01 13:24:24 -0700

    Initial Commit

---
+++ Ranger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ranger
+//
+//  Created by Michael Harper on 4/1/19.
+//  Copyright © 2019 Radius Networks, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
